219a98121639a88e300d1810a5a1b8e050c0b455
diaowinner <32033700+diaowinner@users.noreply.github.com>
2022-03-22 15:23:41 +0800

    Update fetch_svg_font.sh

--- fetch_svg_font.sh
+++ fetch_svg_font.sh
@@ -1,1 +1,1 @@
-wget -q `curl -s https://api.github.com/repos/Radically/glyphwiki-gensvg/releases/latest | python3 -c "import sys, json; print('\n'.join(list(map(lambda x: x['browser_download_url'], json.load(sys.stdin)['assets']))))"`
+wget -q `curl -s https://api.github.com/repos/diaowinner/glyphwiki-gensvg/releases/latest | python3 -c "import sys, json; print('\n'.join(list(map(lambda x: x['browser_download_url'], json.load(sys.stdin)['assets']))))"`
